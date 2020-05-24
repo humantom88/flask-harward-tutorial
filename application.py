b184a37794789c71d5616a2e21130605250ffbb0
tommy <humantom88@gmail.com>
2020-05-24 18:29:17 +0300

    File Database added

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
-import os
-import smtplib
+import csv
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -12,21 +11,20 @@ def index():
     return render_template("index.html", name=name)
 
 @app.route("/registered")
-def registrants():
-    return render_template("registered.html", students=["John Doe", "Alice Cooper"])
-
+def registered():
+    with open("registered.csv", "r") as file:
+        reader = csv.reader(file)
+        students = list(reader)
+    return render_template("registered.html", students=students)
 
 @app.route("/register", methods=["POST"])
 def register():
     name = request.form.get("name")
-    email = request.form.get("email")
     dorm = request.form.get("dorm")
     if not name or not dorm:
         return render_template("failure.html")
-    message = "You are registered!"
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(FROM_EMAIL, os.getenv("PASSWORD"))
-    server.sendmail(FROM_EMAIL, email, message)
-
-    return render_template("success_html")
+    with open("registered.csv", "a") as file:
+        writer = csv.writer(file)
+        writer.writerow((name, dorm))
+        file.close()
+    return render_template("success.html")
